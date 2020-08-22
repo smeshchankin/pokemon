97c24d22080e209a5c52d610c6ae4573195bba22
Sergey Meshchankin <smeshchankin@gmail.com>
2020-08-23 01:35:59 +0300

    [PM-002] Add Attack button

--- main.js
+++ main.js
@@ -1,7 +1,60 @@
 (function() {
+    const button = {
+        attack: document.getElementById('attack')
+    };
+
+    pokemons = [
+        {
+            name: document.querySelector('#player .name').textContent,
+            health: document.querySelector('#player .health'),
+            hp: 100
+        },
+        {
+            name: document.querySelector('#enemy .name').textContent,
+            health: document.querySelector('#enemy .health'),
+            hp: 100
+        }
+    ];
+
+    button.attack.addEventListener('click', function () {
+        pokemons.forEach(function(pokemon) {
+            pokemon.hp -= random(10);
+            if (pokemon.hp < 0) {
+                pokemon.hp = 0;
+            }
+        });
+        render();
+
+        if (pokemons[0].hp === 0 && pokemons[1].hp === 0) {
+            alert('Frendship wins!');
+            init();
+        } else if (pokemons[0].hp === 0) {
+            alert( pokemons[1].name + ' win!');
+            init();
+        } else if (pokemons[1].hp === 0) {
+            alert( pokemons[0].name + ' win!');
+            init();
+        }
+
+        render();
+
+        function random(max) {
+            return Math.ceil(Math.random() * max);
+        }
+    });
+
     init();
 
     function init() {
-        console.log('Init match');
+        pokemons.forEach(function(pokemon) {
+            pokemon.hp = 100;
+        });
+        render();
+    }
+
+    function render() {
+        pokemons.forEach(function(pokemon) {
+            pokemon.health.style.width = pokemon.hp + '%';
+        });
     }
 }());
